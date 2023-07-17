96c4808fb5aa71e9dc9a2e9f3ce882b854f9110b
gonzam77 <gonzam77@gmial.com>
2023-07-17 18:29:07 -0300

    api genres url

--- src/controllers/getGenres.js
+++ src/controllers/getGenres.js
@@ -3,8 +3,8 @@ const axios = require("axios");
 const { API_KEY } = process.env;
 
 const getGenres = async () => {
-    const response = await axios(`http://localhost:3001/genres${API_KEY}`);
-    //const response = await axios(`https://api.rawg.io/api/genres?key=${API_KEY}`);
+    //const response = await axios(`http://localhost:3001/genres${API_KEY}`);
+    const response = await axios(`https://api.rawg.io/api/genres?key=${API_KEY}`);
     const data = response.data.results;
     saveGenres(data);
 }
